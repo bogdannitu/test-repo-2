d94b519bb827f11fe8df261ab5daee582afd00ca
LaraGEN <laragen@hehecoding.com>
2023-07-04 11:08:09 +0300

    Commit #0 made by LaraGEN @2023-07-04 08:08:09

---
+++ lang/en/global.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'save' => 'Save',
+    'cancel' => 'Cancel',
+    'edit' => 'Edit',
+    'delete' => 'Delete',
+    'show' => 'Show',
+    'create' => 'Create',
+    'add' => 'Add',
+    'actions' => 'Actions',
+    'choose' => 'Choose'
+];
