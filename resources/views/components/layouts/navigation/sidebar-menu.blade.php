480e761aca4f29bef0a4b0cb6e8569b64bb748d5
LaraGEN <laragen@hehecoding.com>
2023-07-04 11:15:07 +0300

    Commit #62 made by LaraGEN @2023-07-04 08:15:07

--- resources/views/components/layouts/navigation/sidebar-menu.blade.php
+++ resources/views/components/layouts/navigation/sidebar-menu.blade.php
@@ -1,2 +1,8 @@
 <ul class="sidebar__wrapper-menu">
+            <li>
+        <a href="{{ route('laragen.admin.car.index') }}" class="sidebar__wrapper-menu-item {{request()->routeIs('laragen.admin.car.index') ? 'active' : ''}}">
+            <i class="fa fa-cogs sidebar__wrapper-menu-item-icon"></i>
+            <span class="sidebar__wrapper-menu-item-title">car</span>
+        </a>
+    </li>
     </ul>
