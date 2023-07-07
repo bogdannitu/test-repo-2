19d3ee7225241a1c1b8885f9f7e0ddfb04bde3f1
LaraGEN <laragen@hehecoding.com>
2023-07-07 10:20:36 +0300

    Commit #126 made by LaraGEN @2023-07-07 07:20:36

---
+++ resources/views/livewire/generated/backoffice/car/wheel/create.blade.php
@@ -0,0 +1,12 @@
+<x-laragen::form wire:submit.prevent="submit" class="pb-7 bg-white shadow rounded px-7">
+    
+    <div class="form-group">
+
+        <x-laragen::button class="mr-2">        {{ trans('global.save') }}        </x-laragen::button>
+                <a href="{{ route('laragen.admin.car.index') }}" class="btn btn-secondary">
+            {{ trans('global.cancel') }}
+        </a>
+    </div>
+
+</x-laragen::form>
+@includeIf('app.backoffice.wheel.create.footer')
