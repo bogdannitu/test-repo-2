98e507320334bd9f1e9476b9aadcd26beb9c0cc3
LaraGEN <laragen@hehecoding.com>
2023-07-07 09:51:42 +0300

    Commit #115 made by LaraGEN @2023-07-07 06:51:42

--- resources/views/app/generated/backoffice/car/fields/create/color.blade.php
+++
@@ -1,3 +0,0 @@
-<x-laragen::input.group  class="px-2 w-full" label="{{ @trans('cruds.car.fields.color') }}" for="28" :errors="$errors->get('car.color')" >
-    <x-laragen::input.text placeholder="{{ trans('global.add') }} {{ trans('cruds.car.fields.color')}}" name="car.color" id="28" wire:model.defer="car.color" />
-</x-laragen::input.group>
