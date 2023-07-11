3a3e2752cda059851631bdf7a2364028c72d03f8
LaraGEN <laragen@hehecoding.com>
2023-07-11 08:06:05 +0300

    Commit #137 made by LaraGEN @2023-07-11 05:06:05

--- resources/views/livewire/generated/backoffice/car/wheel/create.blade.php
+++ resources/views/livewire/generated/backoffice/car/wheel/create.blade.php
@@ -1,4 +1,27 @@
 <x-laragen::form wire:submit.prevent="submit" class="pb-7 bg-white shadow rounded px-7">
+                    @includeIf('app.backoffice.wheel.create.fieldset.default-fieldset-6.before')
+        <fieldset>
+
+            
+            <div class="flex-1 -mx-2">
+                @includeIf('app.backoffice.wheel.create.fieldset.default-fieldset-6.content_before')
+                                    @includeIf('app.backoffice.wheel.fields.create.color_before')
+                    @includeFirst([
+                                'app.backoffice.wheel.fields.create.color',
+                                'app.generated.backoffice.wheel.fields.create.color'
+                            ])
+                    @includeIf('app.backoffice.wheel.fields.create.color_after')
+                                    @includeIf('app.backoffice.wheel.fields.create.size_before')
+                    @includeFirst([
+                                'app.backoffice.wheel.fields.create.size',
+                                'app.generated.backoffice.wheel.fields.create.size'
+                            ])
+                    @includeIf('app.backoffice.wheel.fields.create.size_after')
+                                @includeIf('app.backoffice.wheel.create.fieldset.default-fieldset-6.content_after')
+            </div>
+
+        </fieldset>
+        @includeIf('app.backoffice.wheel.create.fieldset.default-fieldset-6.after')
     
     <div class="form-group">
 
