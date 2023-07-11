eea172140e6fea99543cec8d06d6de1ee74f84aa
LaraGEN <laragen@hehecoding.com>
2023-07-11 07:37:56 +0300

    Commit #133 made by LaraGEN @2023-07-11 04:37:56

--- app/Http/Livewire/Generated/Backoffice/Car/Wheel/CreateTrait.php
+++ app/Http/Livewire/Generated/Backoffice/Car/Wheel/CreateTrait.php
@@ -21,7 +21,7 @@ trait CreateTrait
 
     public function render()
     {
-                    return view('livewire.generated.backoffice.wheel.car.create');
+                    return view('livewire.generated.backoffice.asd.car.create');
             }
 
     public function submit()
