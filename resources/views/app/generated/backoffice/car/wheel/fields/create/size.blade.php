72d8751857475b042329e8b87f4f56512d84b737
LaraGEN <laragen@hehecoding.com>
2023-07-11 15:22:26 +0300

    Commit #148 made by LaraGEN @2023-07-11 12:22:26

--- resources/views/app/generated/backoffice/car/wheel/fields/create/size.blade.php
+++ resources/views/app/generated/backoffice/car/wheel/fields/create/size.blade.php
@@ -1,3 +1,3 @@
 <x-laragen::input.group required class="px-2 w-full" label="{{ @trans('cruds.wheel.fields.size') }}" for="29" :errors="$errors->get('wheel.size')" >
-    <x-laragen::input.text placeholder="{{ trans('global.add') }} {{ trans('cruds.wheel.fields.size')}}" name="wheel.size" id="29" wire:model.defer="car.size" />
+    <x-laragen::input.text placeholder="{{ trans('global.add') }} {{ trans('cruds.wheel.fields.size')}}" name="wheel.size" id="29" wire:model.defer="wheel.size" />
 </x-laragen::input.group>
