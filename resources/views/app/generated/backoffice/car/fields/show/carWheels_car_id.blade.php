f93de7ad06a61a1c5fb12145cf325e988e10ecfa
LaraGEN <laragen@hehecoding.com>
2023-07-12 18:10:02 +0300

    Commit #197 made by LaraGEN @2023-07-12 15:10:02

--- resources/views/app/generated/backoffice/car/fields/show/carWheels_car_id.blade.php
+++ resources/views/app/generated/backoffice/car/fields/show/carWheels_car_id.blade.php
@@ -1,1 +1,1 @@
-@livewire('app.backoffice.car.wheel.create', ['car' => $car]) @livewire('app.backoffice.car.wheel.index', ['car' => $car])
+ @livewire('app.backoffice.car.wheel.index', ['car' => $car])@livewire('app.backoffice.car.wheel.create', ['car' => $car])
