4d17bf45c233def87770b622ace512c280a64260
LaraGEN <laragen@hehecoding.com>
2023-07-12 17:22:47 +0300

    Commit #181 made by LaraGEN @2023-07-12 14:22:47

---
+++ app/Http/Livewire/App/Backoffice/Car/Wheel/Create.php
@@ -0,0 +1,10 @@
+<?php
+    namespace App\Http\Livewire\App\Backoffice\Car\Wheel;
+
+use Livewire\Component;
+    use App\Http\Livewire\Generated\Backoffice\Car\Wheel\CreateTrait;
+
+class Create extends Component
+{
+    use CreateTrait;
+}
