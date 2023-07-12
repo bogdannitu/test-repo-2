11ac084123f0cb1a029a3ad6bdc25537e5dd70bf
LaraGEN <laragen@hehecoding.com>
2023-07-12 15:13:27 +0300

    Commit #165 made by LaraGEN @2023-07-12 12:13:27

--- resources/views/livewire/generated/backoffice/car/show.blade.php
+++ resources/views/livewire/generated/backoffice/car/show.blade.php
@@ -30,7 +30,21 @@
                     </td>
                 </tr>
                 @includeIf('app.backoffice.car.fields.show.name_after')
-                                                        </table>
+                                                            <tr>
+                    <th class="text-left">
+                        {{ trans('cruds.car.fields.carWheels') }}                    </th>
+                    <td>
+
+                        @includeFirst(
+                            [
+                            'app.backoffice.car.fields.show.carWheels_car_id',
+                            'app.generated.backoffice.car.fields.show.carWheels_car_id'
+                            ]
+                        )
+                    </td>
+                </tr>
+                @includeIf('app.backoffice.car.fields.show.carWheels_car_id_after')
+                    </table>
     </div>
 
 @includeIf('app.backoffice.car.show.footer')
