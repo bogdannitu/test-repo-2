53d71ecaa85eb97afc6c894dfdd722a3fda7e18a
LaraGEN <laragen@hehecoding.com>
2023-07-12 15:12:24 +0300

    Commit #164 made by LaraGEN @2023-07-12 12:12:24

--- resources/views/livewire/generated/backoffice/car/show.blade.php
+++ resources/views/livewire/generated/backoffice/car/show.blade.php
@@ -2,29 +2,35 @@
 
         
         <table class="w-full show-table">
-                                                                                                                                                                                                                                    <tr>
-                        <th class="text-left">
-                            parent stuff
-                        </th>
-                        <td>
-                            deci da
-                        </td>
-                    </tr>
-                                                <tr>
+                                                            <tr>
                     <th class="text-left">
-                        {{ trans('cruds.car.fields.carWheels') }}                    </th>
+                        {{ trans('cruds.car.fields.id') }}                    </th>
                     <td>
 
                         @includeFirst(
                             [
-                            'app.backoffice.car.fields.show.carWheels_car_id',
-                            'app.generated.backoffice.car.fields.show.carWheels_car_id'
+                            'app.backoffice.car.fields.show.id',
+                            'app.generated.backoffice.car.fields.show.id'
                             ]
                         )
                     </td>
                 </tr>
-                @includeIf('app.backoffice.car.fields.show.carWheels_car_id_after')
-                    </table>
+                @includeIf('app.backoffice.car.fields.show.id_after')
+                                                                                                                                                                        <tr>
+                    <th class="text-left">
+                        {{ trans('cruds.car.fields.name') }}                    </th>
+                    <td>
+
+                        @includeFirst(
+                            [
+                            'app.backoffice.car.fields.show.name',
+                            'app.generated.backoffice.car.fields.show.name'
+                            ]
+                        )
+                    </td>
+                </tr>
+                @includeIf('app.backoffice.car.fields.show.name_after')
+                                                        </table>
     </div>
 
 @includeIf('app.backoffice.car.show.footer')
