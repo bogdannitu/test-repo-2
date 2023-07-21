9d462a641272159180d4c9069432c381b9cfd0ed
LaraGEN <laragen@hehecoding.com>
2023-07-21 07:29:34 +0300

    Commit #210 made by LaraGEN @2023-07-21 04:29:34

--- app/Http/Livewire/App/Backoffice/Car/Wheel/Edit.php
+++ app/Http/Livewire/App/Backoffice/Car/Wheel/Edit.php
@@ -1,8 +1,8 @@
 <?php
-namespace App\Http\Livewire\App\Backoffice\Car;
+    namespace App\Http\Livewire\App\Backoffice\Car\Wheel;
 
 use Livewire\Component;
-use App\Http\Livewire\Generated\Backoffice\Car\EditTrait;
+    use App\Http\Livewire\Generated\Backoffice\Car\Wheel\EditTrait;
 
 class Edit extends Component
 {
