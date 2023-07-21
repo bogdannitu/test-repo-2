8b4faa8d76f4c8f501d2ad48e161e5f0090781a3
LaraGEN <laragen@hehecoding.com>
2023-07-21 07:18:51 +0300

    Commit #208 made by LaraGEN @2023-07-21 04:18:51

---
+++ resources/views/livewire/generated/backoffice/car/wheel/edit.blade.php
@@ -0,0 +1,40 @@
+<x-laragen::form wire:submit.prevent="submit" class="pb-7 bg-white shadow rounded px-7">                    @includeIf('app.backoffice.wheel.edit.fieldset.default-fieldset-6.before')
+        <fieldset>
+
+            
+            <div class="flex-1 -mx-2">
+                @includeIf('app.backoffice.wheel.edit.fieldset.default-fieldset-6.content_before')
+                                                        @includeIf('app.backoffice.wheel.fields.edit.color_before')
+                    @includeFirst([
+                        'app.backoffice.wheel.fields.edit.color',
+                        'app.generated.backoffice.wheel.fields.edit.color'
+                    ])
+                    @includeIf('app.backoffice.wheel.fields.edit.color_after')
+                                                        @includeIf('app.backoffice.wheel.fields.edit.size_before')
+                    @includeFirst([
+                        'app.backoffice.wheel.fields.edit.size',
+                        'app.generated.backoffice.wheel.fields.edit.size'
+                    ])
+                    @includeIf('app.backoffice.wheel.fields.edit.size_after')
+                                                        @includeIf('app.backoffice.wheel.fields.edit.car_car_id_before')
+                    @includeFirst([
+                        'app.backoffice.wheel.fields.edit.car_id',
+                        'app.generated.backoffice.wheel.fields.edit.car_car_id'
+                    ])
+                    @includeIf('app.backoffice.wheel.fields.edit.car_car_id_after')
+                                @includeIf('app.backoffice.wheel.edit.fieldset.default-fieldset-6.content_after')
+            </div>
+
+        </fieldset>
+        @includeIf('app.backoffice.wheel.edit.fieldset.default-fieldset-6.after')
+    
+    <div class="form-group">
+
+        <x-laragen::button class="mr-2">        {{ trans('global.save') }}        </x-laragen::button>
+                <a href="{{ route('laragen.admin.car.index') }}" class="btn btn-secondary">
+            {{ trans('global.cancel') }}
+        </a>
+    </div>
+
+</x-laragen::form>
+@includeIf('app.backoffice.wheel.edit.footer')
